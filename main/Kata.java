5749ab55838bf008697331054d4cce6f6ad6c8e1
Lucas <lucasclaudinocc@gmail.com>
2023-11-08 09:41:43 -0300

    Adding kata resolutions for codewars

--- main/Kata.java
+++ main/Kata.java
@@ -2,6 +2,7 @@ package main;
 
 import java.util.ArrayList;
 import java.util.Objects;
+import javax.swing.*;
 
 public class Kata
 {
@@ -18,14 +19,18 @@ public class Kata
         for (String decimal : decimals) {
             if(!Objects.equals(decimal, decimals.get(decimals.size() - 1))) {
                 answer.append(decimal).append(" + ");
-            }
+            } else {
             answer.append(decimal);
+
+            }
         }
 
         return answer.toString();
     }
 
     public static void main(String[] args) {
-        System.out.println(expandedForm(123));
+        int numb = Integer.parseInt(JOptionPane.showInputDialog("Enter a number: "));
+        System.out.println(numb);
+        JOptionPane.showMessageDialog(null, expandedForm(numb));
     }
 }
